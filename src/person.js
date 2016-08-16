9cf9a31ebba1e7170990d5a06466dcba3392a3f4
PetronellaSimonsbacka <gussimonpe@student.gu.se>
2016-08-16 11:28:50 +0200

    Started over to learn

--- src/person.js
+++ src/person.js
@@ -2,8 +2,3 @@ function Person(attr) {
   this.weight = attr.weight;
   this.height = attr.height;
 };
-
-Person.prototype.calculate_bmi = function() {
-  calculator = new BMICalculator();
-  calculator.metric_bmi(this);
-};
